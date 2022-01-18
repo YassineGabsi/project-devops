77c7d05e6779b0e6c7c141da919302c56f6e8609
fares <fares.hamouda@wisecorp.tn>
2022-01-18 01:19:09 +0100

    add helm

--- scripts/release-helloapp.sh
+++ scripts/release-helloapp.sh
@@ -4,7 +4,7 @@ export KUBECONFIG=$HOME/.kube/kubeconfig
 export HELM_HOST=127.0.0.1:44134
 result=$(eval helm ls | grep helloapp) 
 if [ $? -ne "0" ]; then 
-   helm install --timeout 180m --name helloapp --set image.tag=$TAG charts/helloapp
+   helm install helloapp --timeout 180m  --set image.tag=$TAG charts/helloapp
 else 
    helm upgrade --timeout 180m helloapp --set image.tag=$TAG charts/helloapp
 fi
